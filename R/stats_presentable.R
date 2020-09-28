c1f7673e02bfae36c17b0a13a812b05ed80d7bca
despresj <despresj@mail.gvsu.edu>
2020-09-27 21:23:22 -0400

    added book_data function

--- R/stats_presentable.R
+++ R/stats_presentable.R
@@ -43,3 +43,14 @@ stats_presentable <- function(df, x, group_var1, group_var2, rounding_digits = 1
              Observations  = n()) %>%
     mutate_if(is.numeric, round, digits = rounding_digits)
 }
+
+book_data <- function (chapter = 1, problem = 19) {
+
+  link <- paste0("http://www.cnachtsheim-text.csom.umn.edu/Kutner/Chapter%20%201%20Data%20Sets/CH0",
+                 chapter, "PR", problem,".txt")
+  data <- read.table(link,  header = FALSE, sep = "")
+  colnames(data) <- setNames(nm = c(paste0("X", seq_along(data) - 1)))
+  colnames(data)[1] <- "Y"
+
+  return(data)
+}
